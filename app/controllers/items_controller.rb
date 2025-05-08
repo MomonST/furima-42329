6931a96079641093950f566fffb16bcdfab7fb4e
MomonST <posy-today6d@icloud.com>
2025-05-08 20:36:17 +0900

    ログイン、ログアウト機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,23 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
   end
 
   def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  private
+
+  def move_to_index
+    return if user_signed_in?
+
+    redirect_to action: :index
   end
 end
