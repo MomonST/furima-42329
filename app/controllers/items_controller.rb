4cdb62f334fac4c581e9e524fd64fe6bf6a0eddf
MomonST <posy-today6d@icloud.com>
2025-05-09 20:19:13 +0900

    データの登録
    モデル、テーブル作成
    データ保存
    バリデーション
    アソシエーション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,10 +2,31 @@ class ItemsController < ApplicationController
   # before_action :move_to_index, except: [:index, :show]
 
   def index
+    @item = Item.new
+    @user = User.find(params[:user_id])
+    @items = @user.items.includes(:user)
   end
 
-  # private
+  def new
+    @item = Item.new
+  end
 
+  def create
+    @user = User.find(params[:user_id])
+    @item = @user.items.new(item_params)
+    if @item.save
+     redirect_to '/'
+    else
+      @items = @user.items.includes(:user)
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :delivery_time_id, :item_price, :user).merge(user_id: current_user.id)
+  end
   # def move_to_index
   # return if user_signed_in?
   # redirect_to action: :index
---
+++ app/models/item.rb
@@ -0,0 +1,17 @@
+class Item < ApplicationRecord
+
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :image, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_time_id, presence: true
+  validates :tem_price, presence: true
+  validates :user, presence: true
+
+end
+
