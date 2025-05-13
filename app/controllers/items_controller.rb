72d025ef2849f8acf2b9ae31402a60651ed40317
MomonST <posy-today6d@icloud.com>
2025-05-13 18:23:08 +0900

    updateアクションとエラーメッセージの確認

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  # before_action :move_to_index, except: [:index, :show]
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update] 
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC").includes(:user)
@@ -19,12 +20,18 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-    @item = Item.find(params[:id])
+  def show  
   end
 
   def edit
-    @item = Item.find(params[:id])
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
@@ -32,8 +39,14 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :image, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :delivery_time_id, :item_price, :user).merge(user_id: current_user.id)
   end
-  # def move_to_index
-  # return if user_signed_in?
-  # redirect_to action: :index
-  # end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  
+  def move_to_index
+    if current_user != @item.user   #@item.order.present?は後でつける
+      redirect_to root_path
+    end
+  end
 end
